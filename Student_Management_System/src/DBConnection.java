28b4e6a2e20230e594a16b0db63bcb8e3a36f54d
maheshwaran-tr <maheshwaran.tr2004@gmail.com>
2023-12-02 19:46:05 +0530

    Student Management System

--- Student_Management_System/src/DBConnection.java
+++ Student_Management_System/src/DBConnection.java
@@ -3,9 +3,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String url = DBconfig.url;
-    private static final String userName = DBconfig.userName;
-    private static final String password = DBconfig.password;
+    private static final String url = DBConfig.url;
+    private static final String userName = DBConfig.userName;
+    private static final String password = DBConfig.password;
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url, userName, password);
